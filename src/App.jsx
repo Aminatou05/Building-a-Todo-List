98c48179242bf241ca289017d739d59219b16282
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-04-19 12:45:43 -0700

    done with alab

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import ToDoList from './components/ToDoList'
+
+function App() {
+
+
+  return (
+   <div>
+    <h1>This my only Main Page</h1>
+<ToDoList/>
+    
+   </div>
+
+   
+  )
+}
+
+export default App
